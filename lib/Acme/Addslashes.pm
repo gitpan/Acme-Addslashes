f3484a565b8af6a28c28f5aa135ad25508023926
James Aitken (PAUSE Custodial Account) <modules@perl.org>
2012-04-10 12:45:03 +0000

    Import of JAITKEN/Acme-Addslashes-0.1.1 from CPAN.
    gitpan-cpan-distribution: Acme-Addslashes
    gitpan-cpan-version:      0.1.1
    gitpan-cpan-path:         JAITKEN/Acme-Addslashes-0.1.1.tar.gz
    gitpan-cpan-author:       JAITKEN
    gitpan-cpan-maturity:     released

--- lib/Acme/Addslashes.pm
+++ lib/Acme/Addslashes.pm
@@ -4,6 +4,8 @@ use utf8;
 
 # ABSTRACT: Perl twist on the most useful PHP function ever - addslashes
 
+=encoding utf-8
+
 =head1 NAME
 
 Acme::Addslashes - Perl twist on the most useful PHP function ever - addslashes
@@ -56,7 +58,7 @@ use parent "Exporter";
 
 our @EXPORT_OK = qw(addslashes);
 
-our $VERSION = '0.1.0';
+our $VERSION = '0.1.1';
 
 =head1 FUNCTIONS
 
